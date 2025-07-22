964ef870265bc7ee843f421f1e722ef9cce589df
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-07-23 00:14:41 +0200

    fix: changed ping to hybrid command

--- commands/testing_commands.py
+++ commands/testing_commands.py
@@ -8,21 +8,16 @@ class TestingCommands(commands.Cog):
 
     # ------ SLASH COMMANDS ------
 
-    @app_commands.guilds(discord.Object(id=1390429149512077383))
-    @app_commands.command(name="ping")
-    async def ping(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"Pong! {round(self.bot.latency * 1000)}ms")
-
     @app_commands.command(name="private_echo", description="Echo a message privately.")
     @app_commands.describe(message="The message to echo.")
     async def private_echo(self, interaction: discord.Interaction, message: str):
         await interaction.response.send_message(message, ephemeral=True)
 
-    # ------ PREFIX COMMANDS ------
+    # ------ HYBRID COMMANDS ------
 
-    @commands.command(name="prefix_ping")
-    async def prefixed_ping(self, ctx: commands.Context):
-        await ctx.send("Pong (from prefix command)!")
+    @commands.hybrid_command(name="ping", description="Check bot latency.")
+    async def ping(self, ctx: commands.Context):
+        await ctx.reply(f"Pong! {round(self.bot.latency * 1000)}ms")
 
 async def setup(bot):
     cog = TestingCommands(bot)
