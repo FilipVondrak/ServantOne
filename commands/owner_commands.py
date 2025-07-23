98b7c713c61a684dd288678aa3652e413e1982ed
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-07-23 16:45:03 +0200

    feat: added command to run bash script

--- commands/owner_commands.py
+++ commands/owner_commands.py
@@ -1,4 +1,5 @@
 import discord
+import subprocess
 from discord.ext import commands
 
 class OwnerCommands(commands.Cog):
@@ -15,6 +16,24 @@ class OwnerCommands(commands.Cog):
 
         print(f"Synced {len(synced_gl)} commands globally ({len(synced_test)} totally).")
         await ctx.send(f"Synced {len(synced_gl)} commands globally ({len(synced_test)} totally)")
+
+
+    @commands.command(name="script")
+    @commands.is_owner()
+    async def script(self, ctx: commands.Context, script: str, *args: str) -> None:
+        script_path = f"./scripts/{script}"
+
+        # tries to run the script selected by user
+        # the output is printed to console and also sent back to user
+        try:
+            result = subprocess.run([script_path, *args], capture_output=True, text=True, check=True)
+            print(result.stdout)
+            await ctx.reply(result.stdout)
+        except FileNotFoundError:
+            await ctx.reply("Script not found / invalid script")
+        except Exception as e:
+            print("Unexpected error:", e)
+            await ctx.reply("That didnt work :(")
 
 
 async def setup(bot):
