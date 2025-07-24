f9145cc769b7a14b665648ee4819713907894676
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-07-24 20:07:39 +0200

    feat: added reminder function

--- commands/random_commands.py
+++ commands/random_commands.py
@@ -1,4 +1,6 @@
 import discord
+import re
+import asyncio
 from discord import app_commands
 from discord.ext import commands
 
@@ -14,6 +16,27 @@ class RandomCommands(commands.Cog):
             await ctx.send(f'{member} joined on {member.joined_at}')
         except discord.ext.commands.errors.MemberNotFound:
             await ctx.send("Please specify a valid member")
+
+    @commands.command()
+    async def remind(self, ctx, time: str = "", task: str = "You have been reminded"):
+        time = time.strip().lower()
+
+        if time == "help" or time == "":
+            await ctx.send("Use format like: `1d2h30m10s Task description`")
+            return
+
+        pattern = r'(?:(\d+)d)?(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?'
+        match  = re.fullmatch(pattern, time)
+        days, hours, minutes, seconds = [int(x) if x else 0 for x in match.groups()]
+        total_seconds = days * 86400 + hours * 3600 + minutes * 60 + seconds
+
+        if total_seconds == 0:
+            await ctx.send("You need to specify at least one time unit (e.g., `10s`, `2m`).")
+            return
+
+        await asyncio.sleep(total_seconds)
+        await ctx.send(f"{ctx.author.mention}\n⏰ Reminder: {task}")
+
 
     @commands.command()
     async def avatar(self, ctx, member: str = None):
